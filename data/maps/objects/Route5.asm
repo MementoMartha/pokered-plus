4541cc2661ca1235883acd7b1f65e9a16ce9c59e
Martha Schilling <harleyrayquaza@gmail.com>
2026-03-05 12:21:24 +0000

    Map improvements
    Changed: Pallet Town, Viridian City, Saffron City, Red's House, Copycat's House, Routes 1, 2, 5, 6, 7, 8, 20 & 22.

--- data/maps/objects/Route5.asm
+++ data/maps/objects/Route5.asm
@@ -2,14 +2,14 @@ Route5_Object:
 	db $a ; border block
 
 	def_warp_events
-	warp_event 10, 29, ROUTE_5_GATE, 4
-	warp_event  9, 29, ROUTE_5_GATE, 3
-	warp_event 10, 33, ROUTE_5_GATE, 1
-	warp_event 17, 27, UNDERGROUND_PATH_ROUTE_5, 1
+	warp_event 10, 31, ROUTE_5_GATE, 4
+	warp_event  9, 31, ROUTE_5_GATE, 3
+	warp_event 10, 35, ROUTE_5_GATE, 1
+	warp_event 15, 29, UNDERGROUND_PATH_ROUTE_5, 1
 	warp_event 10, 21, DAYCARE, 1
 
 	def_bg_events
-	bg_event 17, 29, TEXT_ROUTE5_UNDERGROUND_PATH_SIGN
+	bg_event 13, 29, TEXT_ROUTE5_UNDERGROUND_PATH_SIGN
 
 	def_object_events
 
--- data/maps/objects/Route8.asm
+++ data/maps/objects/Route8.asm
@@ -13,18 +13,18 @@ Route8_Object:
 	db $2c ; border block
 
 	def_warp_events
-	warp_event  1,  9, ROUTE_8_GATE, 1
-	warp_event  1, 10, ROUTE_8_GATE, 2
-	warp_event  8,  9, ROUTE_8_GATE, 3
-	warp_event  8, 10, ROUTE_8_GATE, 4
-	warp_event 13,  3, UNDERGROUND_PATH_ROUTE_8, 1
+	warp_event  1, 10, ROUTE_8_GATE, 1
+	warp_event  1, 11, ROUTE_8_GATE, 2
+	warp_event  8, 10, ROUTE_8_GATE, 3
+	warp_event  8, 11, ROUTE_8_GATE, 4
+	warp_event 13,  5, UNDERGROUND_PATH_ROUTE_8, 1
 
 	def_bg_events
-	bg_event 17,  3, TEXT_ROUTE8_UNDERGROUND_SIGN
+	bg_event 11,  5, TEXT_ROUTE8_UNDERGROUND_SIGN
 
 	def_object_events
-	object_event  8,  5, SPRITE_SUPER_NERD, STAY, RIGHT, TEXT_ROUTE8_SUPER_NERD1, OPP_SUPER_NERD, 3
-	object_event 13,  9, SPRITE_GAMBLER, STAY, UP, TEXT_ROUTE8_GAMBLER1, OPP_GAMBLER, 5
+	object_event  8,  8, SPRITE_SUPER_NERD, STAY, RIGHT, TEXT_ROUTE8_SUPER_NERD1, OPP_SUPER_NERD, 3
+	object_event 13, 11, SPRITE_GAMBLER, STAY, UP, TEXT_ROUTE8_GAMBLER1, OPP_GAMBLER, 5
 	object_event 42,  6, SPRITE_SUPER_NERD, STAY, UP, TEXT_ROUTE8_SUPER_NERD2, OPP_SUPER_NERD, 4
 	object_event 26,  3, SPRITE_COOLTRAINER_F, STAY, LEFT, TEXT_ROUTE8_COOLTRAINER_F1, OPP_LASS, 13
 	object_event 26,  4, SPRITE_SUPER_NERD, STAY, RIGHT, TEXT_ROUTE8_SUPER_NERD3, OPP_SUPER_NERD, 5
